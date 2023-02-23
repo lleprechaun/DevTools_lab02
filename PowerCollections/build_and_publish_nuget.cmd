bc93db179e5d97cd1de896ce1a0705018fd334c4
lleprechaun <lera.elganova00@mail.ru>
2023-02-24 03:16:06 +0400

    edit path in build_and_publish

--- PowerCollections/build_and_publish_nuget.cmd
+++ PowerCollections/build_and_publish_nuget.cmd
@@ -1,2 +1,4 @@
 dotnet pack --configuration Release
-dotnet nuget push C:\Users\lerae\Desktop\DevTools_lab02\PowerCollections\PowerCollections\bin\Release\lleprechaun.PowerCollection.*.nupkg --api-key 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 --source "https://nuget.pkg.github.com/lleprechaun/index.json"
+dotnet nuget push \PowerCollections\bin\Release\lleprechaun.PowerCollection.*.nupkg --api-key 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 --source "https://nuget.pkg.github.com/lleprechaun/index.json"
+
+
