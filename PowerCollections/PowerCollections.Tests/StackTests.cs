97642d1b43bc1f060ba4e670200753636aa55f9d
Валерия Елганова д19-191-2 <103368567+lleprechaun@users.noreply.github.com>
2023-03-26 00:03:54 +0400

    update PowerCollections

---
+++ PowerCollections/PowerCollections.Tests/StackTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+
+namespace Wintellect.PowerCollections.Tests
+{
+    [TestClass]
+    public class ConstructorsStackTests
+    {
+        [TestMethod]
+        public void ConstructorStackWithInputLength()
+        {
+            Stack<int> stack = new Stack<int>(5);
+            Assert.AreEqual(0, stack.Count);
+            Assert.AreEqual(5, stack.Capacity);
+        }
+        [TestMethod]
+        public void DefaultConstructorStack()
+        {
+            Stack<string> stack = new Stack<string>();
+            Assert.AreEqual(0, stack.Count);
+            //по дефолту длина стека равна 10
+            Assert.AreEqual(10, stack.Capacity);
+        }
+        [TestMethod]
+        public void ConstructorSteckWithInputArray()
+        {
+            int[] ints = { 1, 2, 3, 4 };
+            Stack<int> stack = new Stack<int>(ints);
+            Assert.AreEqual(ints.Length, stack.Count);
+            Assert.AreEqual(ints.Length, stack.Capacity);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException), "Длина стека не может быть меньше нуля")]
+        public void ExceptionLengthStackNotLess0()
+        {
+            Stack<bool> stack = new Stack<bool>(-10);
+        }
+    }
+    [TestClass]
+    public class MethodsStackTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException), "Стек переполнен")]
+        public void ExceptionStackOverflow()
+        {
+            Stack<bool> stack = new Stack<bool>(0);
+            stack.Push(true);
+            Assert.AreEqual(0, stack.Count);
+            Assert.AreEqual(0, stack.Capacity);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException), "Стек пуст")]
+        public void PopExceptionStackIsEmpty()
+        {
+            Stack<bool> stack = new Stack<bool>(0);
+            stack.Pop();
+            Assert.AreEqual(0, stack.Count);
+            Assert.AreEqual(0, stack.Capacity);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException), "Стек пуст")]
+        public void TopExceptionStackIsEmpty()
+        {
+            Stack<bool> stack = new Stack<bool>(0);
+            stack.Top();
+            Assert.AreEqual(0, stack.Count);
+            Assert.AreEqual(0, stack.Capacity);
+        }
+        [TestMethod]
+        public void PushStringAAA_stringBBB_stringCCC_then_PopStringCCC_expected_TopStringBBB()
+        {
+            Stack<string> stack = new Stack<string>(4);
+            stack.Push("AAA");
+            stack.Push("BBB");
+            stack.Push("CCC");
+            Assert.AreEqual("CCC", stack.Pop());
+            Assert.AreEqual("BBB", stack.Top());
+            Assert.AreEqual(2, stack.Count);
+            Assert.AreEqual(4, stack.Capacity);
+        }
+        [TestMethod]
+        public void ExpectedTopBoolTrue()
+        {
+            bool[] bools = { true, false, false, true };
+            Stack<bool> stack = new Stack<bool>(bools);
+            Assert.IsTrue(stack.Top());
+        }
+        [TestMethod]
+        public void ExpectedPopBoolTrue_and_TopBoolFalse()
+        {
+            bool[] bools = { true, false, false, true };
+            Stack<bool> stack = new Stack<bool>(bools);
+            Assert.IsTrue(stack.Pop());
+            Assert.IsFalse(stack.Top());
+        }
+        [TestMethod]
+        public void IEnumerableStack()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            Stack<int> stack = new Stack<int>(arr);
+            Stack<int> stack2 = new Stack<int>();
+            foreach (int item in stack)
+            {
+                stack2.Push(item);
+            }
+            Array.Reverse(arr);
+            stack = new Stack<int>(arr);
+            var expected = stack.toArray();
+            var actual = stack.toArray();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ PowerCollections/PowerCollections/Stack.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+
+namespace Wintellect.PowerCollections
+{
+    class Stack<T>
+    {
+        private readonly T[] array;
+        private const int defaultLength = 10;
+        private int count = 0;
+
+        //конструктор с параметром, задающим размер внутреннего хранилища
+        // по дефолту стек создается с макс размером 10
+        public Stack()
+        {
+            array = new T[defaultLength];
+        }
+        //стек с заданной длиной
+        public Stack(int length)
+        {
+            if(length < 0)
+            {
+                throw new InvalidOperationException("Длина стека не может быть меньше нуля");
+            }
+            array = new T[length];
+        }
+        public Stack(params T[] arr)
+        {
+            array = arr;
+            count = arr.Length;
+        }
+        public T this[int x]
+        { 
+            get
+            {
+                return array[x];
+            }
+        }
+
+        //свойство, максимальный размер стека
+        public int Capacity { get { return array.Length; } }
+
+        //свойство, текущее число элементов в стеке
+        public int Count { get { return count; } }
+
+
+        //добавить элемент на вершину стека
+        public void Push(T element)
+        {
+            if (count == array.Length)
+            {
+                throw new InvalidOperationException("Стек переполнен");
+            }
+            array[count++] = element;
+        }
+
+        //снять элемент с вершины и вернуть его значение
+        public T Pop()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Стек пуст");
+            }
+            var element = array[--count];
+            array[count] = default(T);
+            return element;
+        }
+
+        //вернуть значение элемента на вершине стека, но не извлекать
+        public T Top()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Стек пуст");
+            }
+            return array[count - 1];
+        }
+        public T[] toArray()
+        {
+            return array;
+        }
+        //
+        public IEnumerator GetEnumerator()
+        {
+            return new Enumerator<T>(array, count);
+        }
+    }
+
+    class Enumerator<T> : IEnumerator
+    {
+        private readonly T[] array;
+        private int position;
+        private int count;
+
+        public Enumerator(T[] arr, int coun)
+        {
+            array = arr;
+            position = coun;
+            count = coun;
+        }
+        public object Current
+        {
+            get
+            {
+                if (position == count || position < 0)
+                    throw new ArgumentException();
+                return array[position];
+            }
+        }
+        public bool MoveNext()
+        {
+            if (position > 0)
+            {
+                position--;
+                return true;
+            }
+            else
+                return false;
+        }
+        public void Reset() => position = count;
+    }
+}
