19d644e3d6290e1b2e228cccf16037a3e75c1c7f
lleprechaun <lera.elganova00@mail.ru>
2023-03-26 21:56:09 +0400

    task02

---
+++ PowerCollections/PowerCollections.Tests/ExceptionTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Wintellect.PowerCollections.Tests
+{
+	[TestClass]
+	public class ExceptionTest
+	{
+		[TestMethod]
+		public void ThrowException()
+		{
+			throw new Exception();
+		}
+	}
+}
